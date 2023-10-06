6271a53e5b647a6f11eb0998602b20a3f818a306
Kieran Mc Gowan <kieranmcgowan21@hotmail.com>
2023-10-06 21:31:23 +0100

    adding a demo

---
+++ run.sh
@@ -0,0 +1,22 @@
+kubectl cluster-info
+kubectl get nodes
+kubectl get pods 
+
+echo "Apply deployment"
+kubectl apply -f deployment.yaml 
+sleep 30
+kubectl get deployments
+kubectl get pods
+
+echo "Apply service LB"
+kubectl apply -f service.yaml 
+kubectl get services coffee-service
+sleep 10
+curl http://localhost/
+
+echo "Delete service LB"
+kubectl delete -f service.yaml 
+echo "Delete service LB"
+kubectl delete -f deployment.yaml 
+sleep 30
+kubectl get pods 
